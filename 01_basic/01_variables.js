be6880b59cb2cd56ea02e444ecae00038e9a9380
jangidmo <153072312+jangidmo@users.noreply.github.com>
2024-06-03 09:45:26 +0000

    changes done

---
+++ 01_basic/01_variables.js
@@ -0,0 +1,12 @@
+const accountid = 123456
+let accountemail ="mohitjangid958@gmail.com"
+var accountpassword = "10251245721540"
+accountcity ="jaipur"
+
+accountemail="jangidmohit958@gmail.com"
+accountpassword ="6375938797"
+accountcity ="partap nagar"
+
+// console.log(accountemail);
+
+console.table([accountid , accountemail , accountpassword , accountcity])
---
+++ 01_basic/02_datatype.js
@@ -0,0 +1,21 @@
+"use strict" // treat all js code as new version 
+
+// alert(3+3); we are useing node js not browser 
+
+let name = "mohit jangid";
+let age = 27;
+let isloggedin = false;
+
+
+// number => 2 to power 50
+// bigint
+// string =>""
+// booelan => true/false
+// null => stand alone value
+// undefined =>
+// symbol => unique
+
+
+// objects
+
+console.log(typeof false);
